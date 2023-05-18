681bfe65f1e9a0a106fe629d5a1a8d50414db99e
Pablo Tomas Espil <tomasynaza@gmail.com>
2023-05-18 19:20:27 -0300

    prototipo completo

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -20,7 +20,7 @@ export default function Product() {
             setLoading(true);
             const response = await fetch(`http://localhost:1337/api/productos/${id}?populate=*`)
             let respuesta = await response.json();
-            setProduct(respuesta);
+            setProduct(respuesta.data);
             setLoading(false);
             console.log('respuesta',respuesta);
         }
@@ -51,22 +51,22 @@ export default function Product() {
       <>
         <div className='col-md-6'>
           <br></br>
-            <img src={product.attributes.imagen.data[0].attributes.url} height="400px" width="400px"></img>
+          {<img src={product.attributes ? `http://localhost:1337${product.attributes.imagen.data[0].attributes.url}` : ''} height="400px" width="400px"></img>}
         </div>
         <div className='col-md-6'>
             <p className='lead text-black-50'>
-              {''}
+              {product.attributes ? product.attributes.nombre : ''}
             </p>
             <p className='lead my-1'>
-              {''} <i className='fa fa-star'></i>
+              {product.attributes ? product.attributes.categorias.data[0].attributes.Nombre : ''} <i className='fa fa-star'></i>
             </p>
             <h1 className='display-5'>{''}</h1>
             <h3 className='display-4 my-4'>
-              $ {''}
+              $ {product.attributes ? product.attributes.precio : ''}
             </h3>
             <br></br>
             <h5>Descripción</h5>
-            <p className='lead'>{''}</p><br></br>
+            <p className='lead'>{product.attributes ? product.attributes.descripcion : ''}</p><br></br>
             <div class="d-grid gap-2">
             <button class="btn btn-dark" onClick={()=>addProduct('')}>Agregar al carrito</button>
             <NavLink to="/cart" class="btn btn-dark fw-bold" type="button">Ver</NavLink>
